5357daf4d7807ac81a2cda1ef8d9f90934e3b937
Dauerbrot <antrophob17@gmail.com>
2020-07-15 22:47:49 +0200

    3 files modified

--- src/app/patientlistmock.service.ts
+++ src/app/patientlistmock.service.ts
@@ -1,23 +1,23 @@
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 
 @Injectable()
-export class PatientListMockService{
+export class PatientListMockService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient){
+  getPatientList(): Observable<any> {
+    return this.http.get("/assets/patientlist.json");
   }
 
-  getPatientList():Observable<any>{
-  return this.http.get('/assets/patientlist.json');
+  getPatientById(id: string): Observable<any> {
+    return this.http.get("/assets/patients.json")
+      .pipe(map((res: any[]) => res.find(item => item.id === id)));
   }
 
-  getPatients():Observable<any>{
-  return this.http.get('/assets/patients.json');
+  getMedicationByPatientId(patientId: string): Observable<any> {
+    return this.http.get("/assets/medication.json")
+      .pipe(map((res: any[]) => res[patientId]));
   }
-
-  getMedication():Observable<any>{
-  return this.http.get('/assets/medication.json');
-  }
-
 }
