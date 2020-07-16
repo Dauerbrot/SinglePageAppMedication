5bf503896b5ce766cb1e2731e952de7db76fa138
Dauerbrot <antrophob17@gmail.com>
2020-07-16 21:41:14 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PatientListService, PatientListMockService } from './patientlistmock.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule  ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, NgbModule.forRoot()  ],
   declarations: [ AppComponent, HelloComponent ],
   providers: [{provide: PatientListService, useClass: PatientListMockService}],
   bootstrap:    [ AppComponent ]
